b07dec9d70fa8cf7c46df1af5a6c349bc47b5c88
Jeter <jeteryan145@gmail.com>
2026-02-02 21:58:43 -0300

    Expressão condicional ternária

---
+++ src/Aula47.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+
+public class Aula47 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        double preco = 34.5;
+
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+        System.out.println("Desconto: " + desconto);
+
+    }
+}
