27b0febc38bab86d21d8de5040d0d2728ba0b5ae
Jeter <jeteryan145@gmail.com>
2026-02-24 21:29:33 -0300

    ATT: Added get and set

--- src/ProjectConstructor/intitiesConstructor/ProductConstructor.java
+++ src/ProjectConstructor/intitiesConstructor/ProductConstructor.java
@@ -2,17 +2,38 @@ package ProjectConstructor.intitiesConstructor;
 
 public class ProductConstructor {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public ProductConstructor(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
+
     public ProductConstructor(String name, double price) {
         this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
