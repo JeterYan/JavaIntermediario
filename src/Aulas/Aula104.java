7ba393af2b5c3f0328e85a99bd4c033c5e3a8975
Jeter <jeteryan145@gmail.com>
2026-03-12 19:53:44 -0300

    Atualizaçao exercicio.

---
+++ src/Aulas/Aula104.java
@@ -0,0 +1,12 @@
+package Aulas;
+
+public class Aula104 {
+    static void main(String[] args) {
+
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+
+        for (String s : vect) {
+            System.out.println(s);
+        }
+    }
+}
